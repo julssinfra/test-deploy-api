9f8eb0409ec20c6ca1ec63e782c90f6617287fd8
JuliAleja <juliethsierra@infofintech.com.co>
2026-08-09 12:24:54 -0500

    add root and health endpoints

--- main.go
+++ main.go
@@ -4,10 +4,18 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "api-identity ok"})
 	})
-	router.Run() // listens on 0.0.0.0:8080 by default
+
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "ok"})
+	})
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	router.Run()
 }
